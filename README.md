464ae0c945d40f2b4dc7ab65578a03826b3c3b9c
Searchstars <searchstars@163.com>
2023-08-14 15:43:39 +0800

    尝试新增RSA支持

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # RizGoNative
 RizGo的嵌入式版本，适用于ESP32全系芯片（Maybe support esp8266?），更多消息待公开
+
+## 开源项目
--- main/cryptotool.cpp
+++ main/cryptotool.cpp
@@ -1,8 +1,15 @@
+#define OS_RNG_AVAILABLE
+#define CRYPTOPP_DISABLE_ASM
+
 #include <aes.h>
 #include <string>
 #include <array>
 #include <algorithm>
 #include <base64.h>
+#include <cryptopp/rsa.h>
+#include <cryptopp-pem/pem.h>
+#include <cryptopp/base64.h>
+#include <cryptopp/osrng.h>
 
 // A function to pad the text with PKCS7 padding scheme
 std::string pkcs7_pad(const std::string& text, int block_size) {
@@ -24,4 +31,27 @@ std::string aes_256_cbc_encrypt(const std::string& key, const std::string& iv, c
     std::string base64_ciphertext = base64_encode(buffer, padded_text.size()); // Encode the encrypted buffer with base64
     delete[] buffer; // Free the buffer memory
     return base64_ciphertext; // Return the base64 encoded ciphertext
+}
+
+// 输入PEM公钥文件的内容（std::string）和要加密的明文（std::string）
+// 返回base64编码的加密结果（std::string）
+std::string rsa_encrypt_pkcs1(const std::string& pem_public_key, const std::string& plain_text)
+{
+    // 创建RSA公钥对象
+    CryptoPP::RSA::PublicKey public_key;
+    // 从PEM字符串中加载公钥
+    CryptoPP::StringSource ss(pem_public_key, true);
+    CryptoPP::PEM_Load(ss, public_key);
+    // 创建PKCS1 v1.5加密方案对象
+    CryptoPP::RSAES_PKCS1v15_Encryptor encryptor(public_key);
+    // 创建随机数生成器
+    CryptoPP::AutoSeededRandomPool rng;
+    // 创建密文缓冲区
+    std::string cipher_text;
+    // 使用公钥和PKCS1 v1.5方案加密明文
+    CryptoPP::StringSource(plain_text, true, new CryptoPP::PK_EncryptorFilter(rng, encryptor, new CryptoPP::StringSink(cipher_text)));
+    // 对密文进行base64编码
+    std::string base64_cipher_text = base64_encode((BYTE*)cipher_text.data(), cipher_text.size());
+    // 返回base64编码的密文
+    return base64_cipher_text;
 }
